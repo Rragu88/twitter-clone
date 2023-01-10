5b4cfe85b9afd808643bd1b42d51868b092a402e
Ryan Ragusa <42756955+Rragu88@users.noreply.github.com>
2023-01-10 12:46:12 -0600

    updated app.js with changes

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 import { tweetsData } from './data.js';
 
 const tweetInput = document.getElementById('tweet-input');
-const tweetBtn = document.getElementById('tweet-btn');
 
 document.addEventListener('click', (e) => {
-    if (e.target.dataset.comment) {
-        console.log(e.target.dataset.comment);
+    if (e.target.dataset.reply) {
+       handleReplyClick(e.target.dataset.reply);
     } else if (e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like);
     } else if (e.target.dataset.retweet){
-        console.log(e.target.dataset.retweet);
+        handleRetweetClick(e.target.dataset.retweet);
+    } else if (e.target.id === 'tweet-btn') {
+        handleTweetBtnClick();
     }
-});
-
-tweetBtn.addEventListener('click', () => {
-    console.log(tweetInput.value);
 });
 
 function handleLikeClick(tweetId) {
@@ -31,9 +28,59 @@ function handleLikeClick(tweetId) {
     render();
 }
 
+function handleRetweetClick(tweetId) {
+    const targetTweetObj = tweetsData.filter((tweet) => {
+        return tweet.uuid === tweetId;
+    })[0];
+
+    if (targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets--;
+    } else {
+        targetTweetObj.retweets++;
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+    render();
+}
+
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden');
+}
+
+function handleTweetBtnClick() {
+    if (tweetInput.value === '') {
+        return;
+    } else {
+        console.log(tweetInput.value);
+    }
+    
+}
+
 function getFeedHtml(tweets) {
     let feedHtml = ``;
     tweets.forEach((tweet) => {
+        let likeIconClass = '';
+        let repliesHtml = ``;
+        if (tweet.isLiked) {
+            likeIconClass = 'liked';
+        }
+        let retweetIconClass = '';
+        if (tweet.isRetweeted) {
+            retweetIconClass = 'retweeted';
+        }
+        if (tweet.replies.length > 0) {
+            tweet.replies.forEach((reply) => {
+                repliesHtml += `
+                <div class="tweet-reply">
+                    <div class="tweet-inner">
+                        <img src="${reply.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${reply.handle}</p>
+                            <p class="tweet-text">${reply.tweetText}</p>
+                        </div>
+                    </div>
+                </div>`;
+            });
+        }
         feedHtml += `
         <div class="tweet">
             <div class="tweet-inner">
@@ -43,20 +90,23 @@ function getFeedHtml(tweets) {
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
-                            <i class="fa-regular fa-comment-dots" data-comment="${tweet.uuid}"></i>
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                            <i class="fa-solid fa-heart ${likeIconClass}" data-like="${tweet.uuid}"></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                            <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
                 </div>            
             </div>
+        </div>
+        <div class="hidden" id="replies-${tweet.uuid}">
+            ${repliesHtml}
         </div>`; 
     });
     return feedHtml;
